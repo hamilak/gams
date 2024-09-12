a8087c6bcc9521e395b10455f304660c59316ea0
Kalimah <69271526+hamilak@users.noreply.github.com>
2024-09-12 03:15:19 +0100

    reverse flex

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,12 +17,12 @@ const About = () => {
                         <img className='lg:w-96 w-72' src={GovandBoy} alt="Image" />
                     </div>
                 </div>
-                <div className='lg:flex justify-between gap-20 items-center m-10'>
-                    <div className='flex justify-center'>
-                        <img className='lg:w-96 w-72' src={GovandBoy} alt="Image" />
-                    </div>
+                <div className='lg:flex flex-row-reverse justify-between gap-20 items-center m-10'>
                     <div className='lg:w-3/5'>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo in sapiente ex nam eius nulla delectus dicta ducimus porro eum. Reprehenderit, neque expedita placeat ex possimus deserunt harum adipisci modi!</p>
+                    </div>
+                    <div className='flex justify-center'>
+                        <img className='lg:w-96 w-72' src={GovandBoy} alt="Image" />
                     </div>
                 </div>
             </div>
